86c9afa65f1ff5fcf68e639bf0133db1b36bddf2
ctfuller98 <ful17005@byui.edu>
2021-07-06 17:15:51 -0600

    added attack image constants

--- game/constants.py
+++ game/constants.py
@@ -6,14 +6,13 @@ SCREEN_WIDTH = 800
 CENTER_X = SCREEN_WIDTH / 2
 CENTER_Y = SCREEN_HEIGHT / 2
 GRAVITY = 1
-# ANIMAL CONSTANTS
+# SPRITE CONSTANTS
 PLAYER_JUMP_SPEED = 15
 PLAYER_ANIMATION_RATE = 3
 PROJECT_ROOT = os.path.dirname(__file__)
 PLAYER_PATH = os.path.join(PROJECT_ROOT, "assets/KOO/Sprites")
-PLAYER_FALLING = [None] * 2 
-PLAYER_FALLING[0] = arcade.load_texture(f"{PLAYER_PATH}/Fall/Fall1.png")
-PLAYER_FALLING[1] = arcade.load_texture(f"{PLAYER_PATH}/Fall/Fall2.png")
+
+#IDLE
 PLAYER_IDLE = [None] * 8
 PLAYER_IDLE[0] = arcade.load_texture(f"{PLAYER_PATH}/Idle/Idle1.png")
 PLAYER_IDLE[1] = arcade.load_texture(f"{PLAYER_PATH}/Idle/Idle2.png")
@@ -23,6 +22,8 @@ PLAYER_IDLE[4] = arcade.load_texture(f"{PLAYER_PATH}/Idle/Idle5.png")
 PLAYER_IDLE[5] = arcade.load_texture(f"{PLAYER_PATH}/Idle/Idle6.png")
 PLAYER_IDLE[6] = arcade.load_texture(f"{PLAYER_PATH}/Idle/Idle7.png")
 PLAYER_IDLE[7] = arcade.load_texture(f"{PLAYER_PATH}/Idle/Idle8.png")
+
+#JUMPING
 PLAYER_JUMPING = [None] * 2
 PLAYER_JUMPING[0] = arcade.load_texture(f"{PLAYER_PATH}/Jump/Jump1.png")
 PLAYER_JUMPING[1] = arcade.load_texture(f"{PLAYER_PATH}/Jump/Jump2.png")
@@ -35,6 +36,26 @@ PLAYER_WALKING[4] = arcade.load_texture(f"{PLAYER_PATH}/Run/Run5.png")
 PLAYER_WALKING[5] = arcade.load_texture(f"{PLAYER_PATH}/Run/Run6.png")
 PLAYER_WALKING[6] = arcade.load_texture(f"{PLAYER_PATH}/Run/Run7.png")
 PLAYER_WALKING[7] = arcade.load_texture(f"{PLAYER_PATH}/Run/Run8.png")
+
+#FALLING
+PLAYER_FALLING = [None] * 2 
+PLAYER_FALLING[0] = arcade.load_texture(f"{PLAYER_PATH}/Fall/Fall1.png")
+PLAYER_FALLING[1] = arcade.load_texture(f"{PLAYER_PATH}/Fall/Fall2.png")
+
+#ATTACK 1 
+ATTACK1 = [None]*4
+ATTACK1[0] = arcade.load_texture(f"{PLAYER_PATH}/Attack1/Attack1.1.png")
+ATTACK1[1] = arcade.load_texture(f"{PLAYER_PATH}/Attack1/Attack1.2.png")
+ATTACK1[2] = arcade.load_texture(f"{PLAYER_PATH}/Attack1/Attack1.3.png")
+ATTACK1[3] = arcade.load_texture(f"{PLAYER_PATH}/Attack1/Attack1.4.png")
+
+#ATTACK 2 
+ATTACK2 = [None]*4
+ATTACK2[0] = arcade.load_texture(f"{PLAYER_PATH}/Attack2/Attack2.1.png")
+ATTACK2[1] = arcade.load_texture(f"{PLAYER_PATH}/Attack2/Attack2.2.png")
+ATTACK2[2] = arcade.load_texture(f"{PLAYER_PATH}/Attack2/Attack2.3.png")
+ATTACK2[3] = arcade.load_texture(f"{PLAYER_PATH}/Attack2/Attack2.4.png")
+
 
 # GROUND CONSTANTS
 
