72039c8a9aea7c567282f62a4a0b6af20b1ca0ae
ELIS-MOBILE-BAT\eli <eli@knisley.info>
2021-07-13 16:34:25 -0600

    fixed attacks

--- game/handle_attack_collisions_action.py
+++ game/handle_attack_collisions_action.py
@@ -13,11 +13,11 @@ class HandleAttackCollisionsAction(Action):
     def _handle_attack_collisions(self, cast):
         player1 = cast.get_actors("players")[0]
         player2 = cast.get_actors("players")[1]
-        arcade.check_for_collision(player1, player2)  
+        if arcade.check_for_collision(player1, player2):
 
-        if(player1.is_hitting()):
-            player2.damage(constants.DEFAULT_ATTACK_DAMAGE)
+            if(player1.is_hitting()):
+                player2.damage(constants.DEFAULT_ATTACK_DAMAGE)
         
-        if(player2.is_hitting()):
-            player1.damage(constants.DEFAULT_ATTACK_DAMAGE)
+            if(player2.is_hitting()):
+                player1.damage(constants.DEFAULT_ATTACK_DAMAGE)
          
