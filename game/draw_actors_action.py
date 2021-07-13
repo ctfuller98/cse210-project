b6bd1e52d4ade88e5720abccece34d76a2b01d4d
DallinPoole66 <72474685+DallinPoole66@users.noreply.github.com>
2021-07-12 18:10:24 -0600

    Added a new tileset

--- game/draw_actors_action.py
+++ game/draw_actors_action.py
@@ -7,11 +7,11 @@ class DrawActorsAction(Action):
         super().__init__()
 
     def execute(self, cast, cue, callback):
+        cast.get_actors("map")[0].draw()
         self._draw_player(cast)
         #self._draw_ground(cast)
         self._draw_instructions(cast)
 
-        cast.get_actors("map")[0].draw()
         
         
     def _draw_ground(self, cast):
--- game/map.py
+++ game/map.py
@@ -4,15 +4,19 @@ class Map():
     
     def __init__(self, map_name, scene):
         
-        # Name of the layer in the file that has our platforms/walls
-        platforms_layer_name = 'Platforms'
 
         # Read in the tiled map
         my_map = arcade.tilemap.read_tmx(map_name)
 
+        
+        self._background = arcade.tilemap.process_layer(map_object=my_map,
+                                                      layer_name="Background",
+                                                      scaling=1,
+                                                      use_spatial_hash=True)
+
         # -- Platforms
         self._platforms = arcade.tilemap.process_layer(map_object=my_map,
-                                                      layer_name=platforms_layer_name,
+                                                      layer_name="Platforms",
                                                       scaling=1,
                                                       use_spatial_hash=True)
         for tile in self._platforms:
@@ -21,6 +25,8 @@ class Map():
         scene.get_cast().add_actor("map", self)
 
     def draw(self):
+        for tile in self._background:
+            tile.draw()
         for tile in self._platforms:
             tile.draw()
              
